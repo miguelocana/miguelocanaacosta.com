6adf5e3009d4df56892e15e56676b782677f966a
miguel <miguelocanaacosta@hotmail.com>
2022-03-06 18:11:50 +0100

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,52 +1,122 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, {useState} from "react"
+import "../styles/styles.scss"
+import Section from "../components/Section"
+import me from "../images/me.jpg"
+import Helmet from "react-helmet"
+import Card from "../components/Card"
+import content from "../content/experience.json"
+import useLocalStorage from "use-local-storage";
+import Toggle from "react-toggle";
+import Tag from "../components/Tag"
+import ContactButton from "../components/ContactButton"
+import ReactTypingEffect from "react-typing-effect";
+import Minisection from "../components/Minisection"
+import Notification from "../components/Notification";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
+
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  
+  const firstLoad = theme === "dark" ? true : false;
+  const [isDark, setIsDark] = useState(firstLoad);
+
+  const handleToggle = (theme) => {
+    if (theme === "dark") {
+      setIsDark(true)
+    } else {
+      setIsDark(false)
+    }
+  };
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    handleToggle(newTheme);
+  };
+
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="app" data-theme={theme}>
+
+      <Helmet>
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+      </Helmet>
+
+      <section class="hero is-fullheight">
+
+        <div class="box" id="wrapper">
+
+          <div class="block" id="header">
+
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+              
+              <div class="navbar-brand">
+                  <span class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-unlock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 9.9-1"></path></svg>
+                  </span>
+              </div>
+
+              <div class="navbar-menu">
+                
+                <div class="navbar-start">
+                  <a class="navbar-item" href="https://github.com/miguelocana" id="user" target="_blank">
+                    miguelocana
+                  </a>
+                  
+                  <span className="navbar-item">
+                    /
+                  </span>
+
+                  <a class="navbar-item" href="http://localhost:8000/" id="readme">
+                    <strong>README.md</strong>
+                  </a>
+                </div>
+
+                <div class="navbar-end">
+                  <div class="navbar-item">
+                    <div class="buttons">
+                      <Toggle
+                        defaultChecked={isDark}
+                        className="toggle"
+                        onChange={switchTheme}
+                        icons={{
+                          checked: null,
+                          unchecked: null
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </nav>
+
+          </div>
+
+          <div class="block" id="content">
+
+            <Section title={null} id="notfound">
+              
+              <p className="title">404</p>
+              <p className="subtitle">Page not found</p>
+              <a href="http://localhost:8000/">← Go back</a>
+            </Section>
+
+          </div>
+
+          <div class="block" id="footer">
+            <div className="container has-text-centered">
+              <a href="https://github.com/miguelocana/miguelocanaacosta.com">
+                Built by Miguel Ocaña
+              </a>
+            </div>
+          </div>
+
+
+        </div>  
+
+      </section>
     </main>
   )
 }
